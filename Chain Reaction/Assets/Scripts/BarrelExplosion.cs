bb08cbd168e8e4b24827203cd4c37a5e52d7fa62
Thomas Becker <thomaswilliambecker@gmail.com>
2018-04-29 13:26:30 -0700

    added in barrel explosion sounds

--- Chain Reaction/Assets/Scripts/BarrelExplosion.cs
+++ Chain Reaction/Assets/Scripts/BarrelExplosion.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BarrelExplosion : MonoBehaviour {
-	// Use this for initialization
-	void Start () {	
+    public AudioClip[] barrelExplosionClips;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        playExplosionSound();
+    }
+
+    // Use this for initialization
+    void Start () {	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void playExplosionSound()
+    {
+        if (barrelExplosionClips.Length > 0)
+        {
+            int index = Random.Range(0, barrelExplosionClips.Length);
+            Debug.Log("playing sound number " + index);
+            AudioClip chosenExplosion = barrelExplosionClips[index];
+            audioSource.PlayOneShot(chosenExplosion);
+        }
+    }
 }
