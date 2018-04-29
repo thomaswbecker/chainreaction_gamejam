1b0369ca22d3d154232ed7ebbd373bdc78f845f7
2014PC\Cesar Gonzalez <tankero@gmail.com>
2018-04-29 04:56:52 -0700

    - Added Scene Level 1 to Scenes folder. Example puzzle with a chain reaction - Added "Shipping Container Environment". Contains standard free assets for industrial environments. Very simple - Added destructible script so non-barrel objects react to explosions without exploding themselves. - Added a capsule collider to Man In Suit prefab. - Revised Barrel script to make it aware of destructible objects. - Tweaked explosion radius variable in Game Settings

---
+++ Chain Reaction/Assets/Destructible.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnDestroy()
+    {
+        Debug.Log("Ouch! I've been killed by a cleverly executed sequence of events!");
+    }
+}
