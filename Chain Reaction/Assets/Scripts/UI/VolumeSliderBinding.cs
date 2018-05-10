4d14b96dba016b40524afc2693428e4959a5f3b0
Carson Lee <keshi@users.noreply.github.com>
2018-05-09 23:45:32 -0700

    volume mixer and slider controls with placeholder blank texture..

---
+++ Chain Reaction/Assets/Scripts/UI/VolumeSliderBinding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class VolumeSliderBinding : MonoBehaviour
+{
+    public Slider slider;
+    public AudioMixer mixer;
+
+    public string AudioParameterID;
+
+    void Start()
+    {
+        float mixerVolume;
+        bool success = mixer.GetFloat(AudioParameterID, out mixerVolume);
+        Debug.Assert(success, "VolumeSliderBinding AudioParameterID is incorrect("+AudioParameterID+")");
+        slider.value = mixerVolume;
+
+    }
+    public void OnVolumeChanged() {
+        mixer.SetFloat(AudioParameterID, slider.value);
+    }
+}
