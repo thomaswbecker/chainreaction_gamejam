193094059e825ce069bf7701336fa80c9852915c
2014PC\Cesar Gonzalez <tankero@gmail.com>
2018-05-19 13:31:56 -0700

    Level 3 fixed

--- Chain Reaction/Assets/BarrelRotator.cs
+++ Chain Reaction/Assets/BarrelRotator.cs
@@ -11,9 +11,6 @@ public class BarrelRotator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        
 
-        transform.Rotate(0, x, 0, Space.World);
     }
 }
